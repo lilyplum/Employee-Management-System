4089b8a39014649a1a877d8b769a72feddc1a147
lilyplum <chen.lily@outlook.de>
2023-10-31 11:38:09 +0100

    add frontend /w react and bootstrap

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import {useState} from 'react'
+import './App.css'
+
+function App() {
+
+    return (
+        <>
+            <div>
+                <h1>React with Bootstrap should be working</h1>
+                <button type={"button"} className={"btn btn-primary"}>hello</button>
+            </div>
+        </>
+    )
+}
+
+export default App
