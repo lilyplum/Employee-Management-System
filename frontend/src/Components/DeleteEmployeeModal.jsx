61a1eba5514b7b591cdbc14af80334ddaf5f0db5
Lily Chen <137886276+lilyplum@users.noreply.github.com>
2023-11-03 17:00:56 +0100

    add delete function in employee modal

--- frontend/src/Components/DeleteEmployeeModal.jsx
+++ frontend/src/Components/DeleteEmployeeModal.jsx
@@ -3,7 +3,7 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import deleteEmployee from "../Functions/deleteEmployee.js";
 
-const DeleteEmployeeModal = ({employee}) => {
+const DeleteEmployeeModal = ({employee, handleDelete}) => {
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
@@ -17,11 +17,11 @@ const DeleteEmployeeModal = ({employee}) => {
             <Modal centered size={"lg"} show={show} onHide={handleClose}>
 
                 <Modal.Header>
-                    <Modal.Title>Editing</Modal.Title>
+                    <Modal.Title>Deleting</Modal.Title>
                 </Modal.Header>
 
                 <Modal.Body>
-                    <p>Are you sure you want to permanently remove {employee.firstName} {employee.lastName} from the
+                    <p>Are you sure you want to permanently remove <b>{employee.firstName.toUpperCase()} {employee.lastName.toUpperCase()}</b> from the
                         database</p>
                 </Modal.Body>
 
@@ -33,7 +33,7 @@ const DeleteEmployeeModal = ({employee}) => {
                         variant="danger"
                         onClick={() => {
                             handleClose();
-                            deleteEmployee(id);
+                            handleDelete(employee.id);
                         }}>
                         Delete
                     </Button>
